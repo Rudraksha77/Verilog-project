c969c5ab89b8126accd9f9a7e914caf6754b73ed
Rudraksha Deulgaonkar <rudrakshadeulgaonkar@gmail.com>
2026-07-11 21:40:00 +0530

    Add files via upload
    Verilog HDL implementation of digital encoder circuits with simulation and testbench for functional verification.

---
+++ Encoder/encoder8_3_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ns
+module encoder8_3_test;
+reg [7:0]in;
+wire [2:0]out;
+
+// instantiate the unit under test (uut)
+encoder8_3 inst_encoder8_3(
+.in(in),
+.out(out)
+);
+
+initial
+begin
+// initialize the input
+in=0;
+
+#20 {in}=8'b00000001;
+#20 {in}=8'b00000010;
+#20 {in}=8'b00000100;
+#20 {in}=8'b00001000;
+
+#20 {in}=8'b00010000;
+#20 {in}=8'b00100000;
+#20 {in}=8'b01000000;
+#20 {in}=8'b10000000;
+#20;
+$finish;
+end
+initial 
+begin 
+$monitor("in=%b out=%b",in,out);
+end
+endmodule
